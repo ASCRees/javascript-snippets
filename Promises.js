bee567b5e60748c54c4472db03f5e564b8d024d7
arees2020 <andrew.rees2020@gmail.com>
2020-05-03 14:49:16 +0100

    feature/Javascript-snippets-02 - Altered promises to include a closure example

--- Promises.js
+++ Promises.js
@@ -1,16 +1,17 @@
-//let val=0;
+// This demonstrates Promises and the fact the entire Call stack is executed before the callback queue is executed.
+
 console.log("Start of Call Stack");
 
 // Create a promise wrapped around a function
 // The value of val is passed into the promise.
 let promise1 = function (val) {
     return new Promise(function (resolve, reject) {
-        console.log("promise1");
+        console.log("In function promise1");
         if (val == 1) {
-            setTimeout(() => resolve('Promise Accepted -' + val), 5000);
+            setTimeout(() => resolve('promise1 - Promise Accepted -' + val), 5000);
             //resolve('Promise Accepted -'+val) 
         } else {
-            reject('Promise Rejected -' + val)
+            reject('promise1 - Promise Rejected -' + val)
         }
     })
 };
@@ -19,7 +20,7 @@ function callPromise(val) {
     //   val++;
 
     // Execute the promise. Pass in val.
-    console.log("Start");
+    console.log("Start callPromise");
     promise1(val)
         .then(function (successMessage) {
             console.log(successMessage);
@@ -32,23 +33,36 @@ function callPromise(val) {
             //error handler function is invoked 
             console.log("Catch - " + errorMessage);
         });
-    console.log("End");
+    console.log("End callPromise");
 }
 
 callPromise(1);
 
-let promisefunc = function () {
+let promisefunc = function (passedInVal) {
+    console.log("Starting promisefunc. PassedInVal =" + passedInVal);
     return new Promise(function (resolve, reject) {
         // the function is executed automatically when the promise is constructed
         console.log("promisefunc");
         // after 1 second signal that the job is done with the result "done"
-        setTimeout(() => resolve(console.log("Done callback")), 5000);
+        setTimeout(() =>
+            //resolve(console.log("Done callback - " ),5000));
+
+            resolve(function outputMessage() {
+                console.log("Executing promisefunc callback. The value of passedInVal is " + passedInVal);
+            }, 5000));
+
     })
 };
 
-promisefunc()
+// This not only shows an example of using a promise but also a closure within a promise. as we pass in "THIS IS MY SECRET" to the outer function.
+// This value is available after the proise is resolved in the function returned by the resolve.
+
+promisefunc("THIS IS MY SECRET")
+    .then(function (closureFunc) {
+        closureFunc(); // This should output "Done callback - THIS IS MY SECRET"
+    })
     .then(function () {
-        console.log("Promisefunc ended");
+        console.log("Promisefunc callback ended");
     });
 
 
@@ -64,3 +78,6 @@ promisefunc()
 })(10000000000);
 
 console.log("End of Call Stack"); // Call back wont occur until after the Call Stack has completed
+console.log("");
+console.log("Now return the Callback Queue");
+console.log("");
